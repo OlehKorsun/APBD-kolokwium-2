008cd31d13f54b6fada262a91a3b07ef8813ca47
Oleh Korsun <oleg.korsun.0505@gmail.com>
2025-06-03 20:48:39 +0200

    Init

---
+++ Kolokwium2/Kolokwium2/Program.cs
@@ -0,0 +1,36 @@
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.IgnoreNullValues = true;
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+});
+
+
+// builder.Services.AddDbContext<ApbdContext>(opt =>
+// {
+//     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+// });
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+// add scoped
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
